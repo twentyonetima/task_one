262198f60b1a89bc341e57640898083368c95ea3
Timofey Petrov <twentyonetima@mail.ru>
2022-04-12 23:58:38 +0300

    Final version

--- task_one/main/views.py
+++ task_one/main/views.py
@@ -11,5 +11,15 @@ def vice_versa(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    words = user_text.split()
+    number_of_words = len(words)
     reversed_text = user_text[::-1]
-    return render(request, 'main/reverse.html', {'usertext': user_text, 'reversedtext':reversed_text})
+    if number_of_words == 1:
+        answer = 'word'
+    else:
+        answer = 'words'
+    return render(request, 'main/reverse.html', {'usertext': user_text,
+                                                 'reversedtext': reversed_text,
+                                                 'numberofwords': number_of_words,
+                                                 'answerwords': answer
+                                                 })
